019a18e190bfa064813b60e114a9800de2c7905a
marcazona <hub@marcazona.com>
2025-07-13 08:53:16 -0500

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,11 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import { AuthProvider, useAuth } from '@/contexts/AuthContext';
+import { AuthProvider, useAuth } from '@/contexts/AuthContext'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   console.log('RootLayout: Component rendering');
   
   console.log('RootLayout: Rendering AuthProvider and Stack');
